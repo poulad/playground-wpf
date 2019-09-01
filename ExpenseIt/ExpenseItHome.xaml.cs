07ef6d410dac816ef9f31efc1341540edbfa4f7b
Poulad <poulad.pld@gmail.com>
2019-09-01 08:29:46 -0400

    Complete Expenseit walkthrough
    https://docs.microsoft.com/en-us/dotnet/framework/wpf/getting-started/walkthrough-my-first-wpf-desktop-application

---
+++ ExpenseIt/ExpenseItHome.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Controls;
+
+namespace ExpenseIt
+{
+    /// <summary>
+    /// Interaction logic for ExpenseItHome.xaml
+    /// </summary>
+    public partial class ExpenseItHome : Page
+    {
+        public ExpenseItHome()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            var expenseReportPage = new ExpenseReportPage(PeopleListBox.SelectedItem);
+            NavigationService.Navigate(expenseReportPage);
+        }
+    }
+}
---
+++ ExpenseIt/ExpenseReportPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Controls;
+
+namespace ExpenseIt
+{
+    /// <summary>
+    /// Interaction logic for ExpenseReportPage.xaml
+    /// </summary>
+    public partial class ExpenseReportPage : Page
+    {
+        public ExpenseReportPage()
+        {
+            InitializeComponent();
+        }
+
+        public ExpenseReportPage(object data)
+            : this()
+        {
+            DataContext = data;
+        }
+    }
+}
